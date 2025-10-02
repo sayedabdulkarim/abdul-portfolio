875de49e252ed9322dac76e4c24ac2b4e919049f
Sayed Abdul <sakarim9124@gmail.com>
2025-10-02 13:53:52 +0530

    Fix Vercel deployment: Remove GitHub Pages config and switch to BrowserRouter

--- src/config/routeConfig.js
+++ src/config/routeConfig.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "../App";
 import HomeScreen from "../screens/auth/Home";
 import AboutScreen from "../screens/auth/About";
